0815886fce72e3ab2b4d8fba463f0211a23d04af
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2024-05-11 23:58:33 +0530

    Time: 37 ms (36.22%), Space: 16.5 MB (88.84%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,13 +1,42 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         
-        # half adder (add 2 binary bits)
         def half_adder(x: int, y: int):
-            # x & y must be single bit
+            # x+y
             sum = x^y
             carry = x&y
-            return 2*carry+sum
+            carry <<= 1
+            if carry == 0:
+                return sum
+            return half_adder(sum, carry)
+
+        def half_subtractor(x: int, y: int):
+            # x-y
+            diff = x^y
+            borrow = (~x)&y
+            borrow <<= 1
+            if borrow == 0:
+                return diff
+            return half_subtractor(diff, borrow)
         
-        return half_adder(a,b)
+        if a==-b:
+            return 0
+        elif a==0 and b!=0:
+            return b
+        elif b==0 and a!=0:
+            return a
+        elif (a>=0 and b>=0) or (a<0 and b<0):  # same sign
+            return half_adder(a, b) 
+        else:
+            if abs(a) < abs(b):
+                a, b = b, a
+            # a is bigger in scale than b
+            diff = half_subtractor(abs(a), abs(b))
+            if b<0: # if smaller val is -ve, total will be +ve
+                return diff
+            else:   # if bigger val is -ve, total will be -ve
+                return -diff
+
+
         
         
