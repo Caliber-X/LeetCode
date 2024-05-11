aa0a63c7ce748cbfe7b1b3360c1f9850b5cc208e
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2024-05-12 00:02:01 +0530

    Time: 38 ms (27.92%), Space: 16.5 MB (36.81%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,11 +1,17 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         
+        if a==-b:
+            return 0
+        elif a==0 and b!=0:
+            return b
+        elif b==0 and a!=0:
+            return a
+
         def half_adder(x: int, y: int):
             # x+y
             sum = x^y
-            carry = x&y
-            carry <<= 1
+            carry = (x&y) << 1
             if carry == 0:
                 return sum
             return half_adder(sum, carry)
@@ -13,29 +19,23 @@ class Solution:
         def half_subtractor(x: int, y: int):
             # x-y
             diff = x^y
-            borrow = (~x)&y
-            borrow <<= 1
+            borrow = ((~x)&y) << 1
             if borrow == 0:
                 return diff
             return half_subtractor(diff, borrow)
         
-        if a==-b:
-            return 0
-        elif a==0 and b!=0:
-            return b
-        elif b==0 and a!=0:
-            return a
-        elif (a>=0 and b>=0) or (a<0 and b<0):  # same sign
+        if (a>=0 and b>=0) or (a<0 and b<0):  # same sign
             return half_adder(a, b) 
         else:
             if abs(a) < abs(b):
                 a, b = b, a
             # a is bigger in scale than b
             diff = half_subtractor(abs(a), abs(b))
-            if b<0: # if smaller val is -ve, total will be +ve
+            # if smaller val is -ve, total will be +ve
+            if b<0:
                 return diff
-            else:   # if bigger val is -ve, total will be -ve
-                return -diff
+            # if bigger val is -ve, total will be -ve
+            return -diff
 
 
         
