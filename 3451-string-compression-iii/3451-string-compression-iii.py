6abe0b0c56aba05eaf89a08aa3354771104948dd
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2024-06-09 20:36:55 +0530

    Time: 406 ms (15.65%), Space: 19.6 MB (75.35%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.py
+++ 3451-string-compression-iii/3451-string-compression-iii.py
@@ -1,12 +1,5 @@
 class Solution:
 
-    def write_char_count(self, count: int, char_: str):
-        q, r = divmod(count, 9)
-        comp = q*f"9{char_}" 
-        if r!=0:
-            comp += f"{r}{char_}"
-        return comp
-
     def compressedString(self, word: str) -> str:
         
         if len(word) == 0:
@@ -15,6 +8,13 @@ class Solution:
         if len(word) == 1:
             return "1"+word
         
+        def write_char_count(count: int, char_: str):
+            q, r = divmod(count, 9)
+            comp = q*f"9{char_}" 
+            if r!=0:
+                comp += f"{r}{char_}"
+            return comp
+
         comp = ""
         count = 1
 
@@ -22,13 +22,13 @@ class Solution:
             current_char, next_char = word[i], word[i+1]
 
             if current_char != next_char:
-                comp += self.write_char_count(count, current_char)
+                comp += write_char_count(count, current_char)
                 count = 1
             
             else:
                 count += 1
 
             if i == len(word)-2:
-                comp += self.write_char_count(count, next_char)
+                comp += write_char_count(count, next_char)
 
         return comp
