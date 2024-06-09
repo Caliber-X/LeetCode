4ff7632a3dae47ac8e2002d8c30c13ee32c58128
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2024-06-09 19:12:41 +0530

    Time: 403 ms (91.89%), Space: 31.8 MB (82.7%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,8 +5,16 @@ class Solution:
         #     return False
         # return True
 
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
+        # nums.sort()
+        # for i in range(1, len(nums)):
+        #     if nums[i] == nums[i-1]:
+        #         return True
+        # return False
+
+        unique_nums = set()
+        for num in nums:
+            if num in unique_nums:
                 return True
+            unique_nums.add(num)
         return False
+        
