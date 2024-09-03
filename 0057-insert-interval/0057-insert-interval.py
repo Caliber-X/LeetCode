5e9d44f1a9a6277772613d9482a89071489beaa4
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2024-09-04 01:45:48 +0530

    Time: 76 ms (51.08%), Space: 19.9 MB (16.5%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -7,17 +7,17 @@ class Solution:
             return [newInterval]
         
         idx = bisect_right(intervals, newInterval[0], key=lambda x:x[0])
-        print(f"{idx=}")
+        # print(f"{idx=}")
         
         i = idx
         while (i < len(intervals)) and (newInterval[1] >= intervals[i][0]):
             i += 1
             # print(f"{i=}")
         # i -= 1
-        print(f"{i=}")
+        # print(f"{i=}")
 
         if idx>0 and i == idx and newInterval[1] <= intervals[idx-1][1]:
-            print("same")
+            # print("same")
             return intervals
 
         intervals_new = []
@@ -25,17 +25,16 @@ class Solution:
             intervals_new = intervals[:idx]
             if len(intervals_new) != 0:
                 intervals_new[-1][1] = max(intervals_new[-1][1], newInterval[1])
-
         else:
             intervals_new = intervals[:idx] + [newInterval]
-        print(f"{intervals_new=}")
+        # print(f"{intervals_new=}")
 
         if i>0 and len(intervals_new) != 0:
             intervals_new[-1][1] = max(intervals_new[-1][1], intervals[i-1][1])
-        print(f"{intervals_new=}")
+        # print(f"{intervals_new=}")
 
         intervals_new += intervals[i:]
-        print(f"{intervals_new=}")
+        # print(f"{intervals_new=}")
 
         return intervals_new
             
