81aaa1cbac232323e75db45590c901c806f206b3
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-06-09 04:07:16 +0530

    Time: 19 ms (87.37%), Space: 23.3 MB (79.61%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,9 +3,11 @@ class Solution:
         n = len(nums)
         result = [1] * n
 
+        # prefix
         for i in range(1, n):
             result[i] = result[i-1] * nums[i-1]
         
+        # postfix
         post_val = 1
         for i in range(n-1, -1, -1):
             result[i] *= post_val
