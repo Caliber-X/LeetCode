3d417efc1cfea7f34520de00422eb2193e2fb08c
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-06-08 17:54:44 +0530

    Time: 523 ms (10.5%), Space: 119.1 MB (7.76%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        
+        rows, cols = len(matrix), len(matrix[0])
+        cache = {}
+
+        def recurse(r, c):
+            if r == rows or c == cols:
+                return 0
+
+            if (r,c) in cache:
+                return cache[(r,c)]
+
+            bottom = recurse(r+1, c)
+            right = recurse(r, c+1)
+            diag = recurse(r+1, c+1)
+
+            cache[(r,c)] = 0
+            if matrix[r][c] == "1":
+                cache[(r,c)] += 1 + min(bottom, right, diag)
+            
+            return cache[(r,c)]
+
+        recurse(0, 0)
+        return max(cache.values()) ** 2
