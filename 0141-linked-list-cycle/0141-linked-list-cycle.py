f1c9e9e78e875cc7dac639920fe24e2dc014aba8
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-06-15 18:51:41 +0530

    Time: 51 ms (39.92%), Space: 19.9 MB (56.41%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -5,14 +5,24 @@
 #         self.next = None
 
 class Solution:
+
+    # # visited using hash map
+    # def hasCycle(self, head: Optional[ListNode]) -> bool:
+    #     visited = set()
+    #     while head is not None:
+    #         if head in visited:
+    #             return True
+    #         visited.add(head)
+    #         head = head.next
+    #     return False
+
+    # 2 pointer, Floyd's Tortoise and Hare
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        visited = set()
-        while head is not None:
-            if head in visited:
+        p_slow, p_fast = head, head
+        while p_fast is not None and p_fast.next is not None:
+            p_slow = p_slow.next
+            p_fast = p_fast.next.next
+            if p_slow == p_fast:
                 return True
-            visited.add(head)
-            head = head.next
         return False
-
-
-        
+            
