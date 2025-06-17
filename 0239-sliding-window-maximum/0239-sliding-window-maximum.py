5a4cf2d94519d2c0cd16df3c36725550cebfd698
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-06-18 01:49:22 +0530

    Time: 161 ms (68.27%), Space: 32.2 MB (21.86%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,11 +4,11 @@ class Solution:
         queue = deque()
         output = []
         for i, n in enumerate(nums):
-            if len(queue) > 0 and queue[0] <= i-k:
-                queue.popleft()
             while len(queue) > 0 and nums[queue[-1]] <= n:
                 queue.pop()
             queue.append(i)
+            if queue[0] <= i-k:
+                queue.popleft()
             if i >= k-1:
                 output.append(nums[queue[0]])
             # print(f"{i=} {n=} {queue=} {output=}")
