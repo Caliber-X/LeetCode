4f0c38fde9555182e54f4d330dc443eaccc9712a
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-06-21 20:21:45 +0530

    Time: 123 ms (32.32%), Space: 27 MB (8.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,20 +1,30 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        day1price, day2price = prices[0], prices[0]
-        max_profit = 0
-        for i in range(1, len(prices)):
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     day1price, day2price = prices[0], prices[0]
+    #     max_profit = 0
+    #     for i in range(1, len(prices)):
 
-            if prices[i] < day1price:
-                day1price, day2price = prices[i], prices[i]
+    #         if prices[i] < day1price:
+    #             day1price, day2price = prices[i], prices[i]
             
-            else:
+    #         else:
 
-                if prices[i] > day2price:
-                    day2price = prices[i]
+    #             if prices[i] > day2price:
+    #                 day2price = prices[i]
 
 
-                if day2price - day1price > max_profit:
-                    max_profit = day2price - day1price
+    #             if day2price - day1price > max_profit:
+    #                 max_profit = day2price - day1price
 
-        return max_profit
+    #     return max_profit
+
+    def maxProfit(self, prices: List[int]) -> int:
+        price_min = prices[0]
+        profit_max = 0
+
+        for price in prices:
+            price_min = min(price_min, price)
+            profit_max = max(profit_max, price-price_min)
+
+        return profit_max
 
