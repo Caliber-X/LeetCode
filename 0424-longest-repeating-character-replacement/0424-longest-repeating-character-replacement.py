4d44384726052baa2c1465939a2c8916713114dd
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-06-22 16:10:26 +0530

    Time: 117 ms (64.87%), Space: 18 MB (48.6%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -27,14 +27,29 @@ class Solution:
     #         max_len = max(max_len, r-l+1)
     #     return max_len
         
-    # O(26.n), better space complexity using array
+    # # O(26.n), better space complexity using array
+    # def characterReplacement(self, s: str, k: int) -> int:
+    #     window = [0] * 26
+    #     l = 0
+    #     max_len = 0
+    #     for r in range(len(s)):
+    #         window[ord(s[r]) - 65] += 1
+    #         while (r-l+1) - max(window) > k:
+    #             window[ord(s[l]) - 65] -= 1
+    #             l += 1
+    #         max_len = max(max_len, r-l+1)
+    #     return max_len
+
+    # O(n), better space complexity using array
     def characterReplacement(self, s: str, k: int) -> int:
         window = [0] * 26
         l = 0
         max_len = 0
+        maxf = 0 
         for r in range(len(s)):
             window[ord(s[r]) - 65] += 1
-            while (r-l+1) - max(window) > k:
+            maxf = max(maxf, window[ord(s[r]) - 65])
+            while (r-l+1) - maxf > k:
                 window[ord(s[l]) - 65] -= 1
                 l += 1
             max_len = max(max_len, r-l+1)
