f8489bab2f1f42d6ca873239d458bf509f5e1e25
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-06-22 16:07:48 +0530

    Time: 235 ms (13.83%), Space: 17.9 MB (86.43%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -11,19 +11,31 @@ class Solution:
     #             l += 1
     #         max_len = max(max_len, r-l+1)
     #     return max_len
-    
-    # O(n)
+
+    # # O(n)
+    # def characterReplacement(self, s: str, k: int) -> int:
+    #     window = {}
+    #     l = 0
+    #     max_len = 0
+    #     maxf = 0 
+    #     for r in range(len(s)):
+    #         window[s[r]] = 1 + window.get(s[r], 0)
+    #         maxf = max(maxf, window[s[r]])
+    #         while (r-l+1) - maxf > k:
+    #             window[s[l]] -= 1
+    #             l += 1
+    #         max_len = max(max_len, r-l+1)
+    #     return max_len
+        
+    # O(26.n), better space complexity using array
     def characterReplacement(self, s: str, k: int) -> int:
-        window = {}
+        window = [0] * 26
         l = 0
         max_len = 0
-        maxf = 0 
         for r in range(len(s)):
-            window[s[r]] = 1 + window.get(s[r], 0)
-            maxf = max(maxf, window[s[r]])
-            while (r-l+1) - maxf > k:
-                window[s[l]] -= 1
+            window[ord(s[r]) - 65] += 1
+            while (r-l+1) - max(window) > k:
+                window[ord(s[l]) - 65] -= 1
                 l += 1
             max_len = max(max_len, r-l+1)
         return max_len
-        
