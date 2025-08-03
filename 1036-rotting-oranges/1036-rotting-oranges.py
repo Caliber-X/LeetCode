44e74b0f6f7d8664ea426ee6e9905e2c7433a713
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-08-03 17:57:25 +0530

    Time: 2 ms (86.45%), Space: 18 MB (26.17%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,12 +1,12 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
+
         M, N = len(grid), len(grid[0])
 
-        def get_fresh_neighbors(r,c):
+        def get_fresh_neighbors(r, c):
             neighbors = []
-            for dr, dc in [(0,1), (1,0), (0,-1), (-1,0)]:
-                nr, nc = r+dr, c+dc
+            for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                nr, nc = r + dr, c + dc
 
                 # out  of bounds
                 if nr < 0 or nr >= M or nc < 0 or nc >= N:
@@ -15,9 +15,9 @@ class Solution:
                 # not a fresh orange
                 if grid[nr][nc] != 1:
                     continue
-                
-                neighbors.append((nr,nc))
-            
+
+                neighbors.append((nr, nc))
+
             return neighbors
 
         count_fresh = 0
@@ -27,31 +27,28 @@ class Solution:
                 if grid[r][c] == 1:
                     count_fresh += 1
                 elif grid[r][c] == 2:
-                    rotten.append((r,c))
+                    rotten.append((r, c))
         # print(f"{count_fresh=} {rotten=}")
 
         # edge cases
-        if len(rotten) == 0 and count_fresh == 0:
+        if count_fresh == 0:
             return 0
-        if len(rotten) == 0:
+        elif len(rotten) == 0:
             return -1
-        elif count_fresh == 0:
-            return 0
-
+        
         # multi source bfs
         count = -1
         while len(rotten) > 0:
-            
+
             count += 1
             rotten_next_level = []
 
-            for r,c in rotten:
-                for nr,nc in get_fresh_neighbors(r,c):
-                    rotten_next_level.append((nr,nc))
+            for r, c in rotten:
+                for nr, nc in get_fresh_neighbors(r, c):
+                    rotten_next_level.append((nr, nc))
                     grid[nr][nc] = 2
                     count_fresh -= 1
-            
+
             rotten = rotten_next_level
 
         return count if count_fresh == 0 else -1
-
