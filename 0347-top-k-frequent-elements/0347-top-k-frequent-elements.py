994d3cfe113a8a027ba0f2255fe2a1aac207608a
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-08-05 00:25:35 +0530

    Time: 5 ms (61.39%), Space: 21.2 MB (61.92%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,49 @@
 import heapq
 
 class Solution:
+
+    # # heap (time_complexity=O(nlogn))
+    # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+    #     frequency_dict = {}
+    #     for i in nums:
+    #         frequency_dict[i] = 1 + frequency_dict.get(i, 0)
+        
+    #     # heap = []
+    #     # for val, freq in frequency_dict.items():
+    #     #     heapq.heappush(heap, (-freq, val))
+
+    #     # output = []
+    #     # for i in range(k):
+    #     #     freq, val = heapq.heappop(heap)
+    #     #     output.append(val)
+
+    #     output = heapq.nlargest(k, frequency_dict.keys(), key=frequency_dict.get)
+
+    #     return output
+
+    # bucket sort (time_complexity=O(n))
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         frequency_dict = {}
         for i in nums:
             frequency_dict[i] = 1 + frequency_dict.get(i, 0)
         
-        # heap = []
-        # for val, freq in frequency_dict.items():
-        #     heapq.heappush(heap, (-freq, val))
-
-        # output = []
-        # for i in range(k):
-        #     freq, val = heapq.heappop(heap)
-        #     output.append(val)
-
-        output = heapq.nlargest(k, frequency_dict.keys(), key=frequency_dict.get)
+        bucket = [None] * len(nums)
+        for val, freq in frequency_dict.items():
+            freq -= 1
+            if bucket[freq] is None:
+                bucket[freq] = []
+            bucket[freq].append(val)
+        
+        output = []
+        count = 0
+        for values in reversed(bucket):
+            if values is None:
+                continue
+            output.extend(values)
+            count += len(values)
+            if count == k:
+                break
 
         return output
