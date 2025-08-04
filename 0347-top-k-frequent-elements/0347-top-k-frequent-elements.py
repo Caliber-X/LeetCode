bdd59497be654a735a80f15f628e7c3157082a72
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-08-05 00:07:44 +0530

    Time: 3 ms (90.58%), Space: 21.2 MB (61.92%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,13 +7,15 @@ class Solution:
         for i in nums:
             frequency_dict[i] = 1 + frequency_dict.get(i, 0)
         
-        heap = []
-        for val, freq in frequency_dict.items():
-            heapq.heappush(heap, (-freq, val))
+        # heap = []
+        # for val, freq in frequency_dict.items():
+        #     heapq.heappush(heap, (-freq, val))
 
-        output = []
-        for i in range(k):
-            freq, val = heapq.heappop(heap)
-            output.append(val)
+        # output = []
+        # for i in range(k):
+        #     freq, val = heapq.heappop(heap)
+        #     output.append(val)
+
+        output = heapq.nlargest(k, frequency_dict.keys(), key=frequency_dict.get)
 
         return output
