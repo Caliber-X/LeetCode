bccc5ebe10e751b43dac1abe64c7b8b9dc81ea2b
Shuvam Deyashi <shuvamdeyashi45@gmail.com>
2025-08-05 00:41:42 +0530

    Time: 64 ms (67.95%), Space: 22.6 MB (66.38%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,22 +1,24 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # heap = []
-        # for point in points:
-        #     x,y = point
-        #     dist = x**2 + y**2
-        #     heapq.heappush(heap, (dist, point))
+        heap = []
+        for point in points:
+            x,y = point
+            dist = x**2 + y**2
+            # heapq.heappush(heap, (dist, point))
+            heap.append((dist, point))
+        heapq.heapify(heap)
 
-        # output = []
-        # for _ in range(k):
-        #     dist, point = heapq.heappop(heap)
-        #     output.append(point)
+        output = []
+        for _ in range(k):
+            dist, point = heapq.heappop(heap)
+            output.append(point)
         
-        # return output
-
-        output = heapq.nsmallest(
-            k,
-            map(lambda point: (point[0]**2 + point[1]**2, point), points),
-            key=lambda x: x[0]
-        )
-        output = [*map(lambda x: x[1], output)]
         return output
+
+        # output = heapq.nsmallest(
+        #     k,
+        #     map(lambda point: (point[0]**2 + point[1]**2, point), points),
+        #     key=lambda x: x[0]
+        # )
+        # output = [*map(lambda x: x[1], output)]
+        # return output
